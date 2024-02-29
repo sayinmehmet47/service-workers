2ff523fe7f253d968dfac8bc6a42699362b8297f
mehmet <sayinmehmet47@gmail.com>
2024-02-29 21:43:30 +0100

    feat:  pwa with fetch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 4;
+const version = 2;
 let staticName = `staticCache$-${version}`;
 let dynamicName = 'dynamicCache';
 let fontName = 'fontCache';
@@ -28,6 +28,69 @@ self.addEventListener('activate', (ev) => {
   );
 });
 
-self.addEventListener('fetch', (ev) => {});
+self.addEventListener('fetch', (ev) => {
+  // ev.request each time the webpage asks for any resource.
+  //Extendable Event
+  console.log(`fetch request for: ${ev.request.url}`);
+  /*                  */
+  // version 1 - pass thru
+  // ev.respondWith(fetch(ev.request));
+  /*                  */
+  // version 2 - check the caches first for the file. If missing do a fetch
+  // ev.respondWith(
+  //   caches.match(ev.request).then((cacheRes) => {
+  //     if (cacheRes == undefined) {
+  //       console.log(`MISSING ${ev.request.url}`);
+  //     }
+  //     return cacheRes || fetch(ev.request);
+  //   })
+  // );
+  /*                  */
+  //version 3 - check cache. fetch if missing. then add response to cache
+  ev.respondWith(
+    caches.match(ev.request).then((cacheRes) => {
+      return (
+        cacheRes ||
+        fetch(ev.request).then((fetchResponse) => {
+          let type = fetchResponse.headers.get('content-type');
+          if (
+            (type && type.match(/^text\/css/i)) ||
+            ev.request.url.match(/fonts.googleapis.com/i)
+          ) {
+            //css to save in dynamic cache
+            console.log(`save a CSS file ${ev.request.url}`);
+            return caches.open(dynamicName).then((cache) => {
+              cache.put(ev.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          } else if (
+            (type && type.match(/^font\//i)) ||
+            ev.request.url.match(/fonts.gstatic.com/i)
+          ) {
+            console.log(`save a FONT file ${ev.request.url}`);
+            return caches.open(fontName).then((cache) => {
+              cache.put(ev.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          } else if (type && type.match(/^image\//i)) {
+            //save in image cache
+            console.log(`save an IMAGE file ${ev.request.url}`);
+            return caches.open(imageName).then((cache) => {
+              cache.put(ev.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          } else {
+            //save in dynamic cache
+            console.log(`OTHER save ${ev.request.url}`);
+            return caches.open(dynamicName).then((cache) => {
+              cache.put(ev.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          }
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener('message', (ev) => {});
